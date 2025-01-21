3061df51d3ad2cf92b42f9b02cf7134899b587e6
Amine Zerouali <zerouali.zerouali@gmail.com>
2025-01-21 11:32:24 -0500

    Mise à jour du .gitignore pour exclure les chemins spécifiques

--- robot/build_simulator.sh
+++
@@ -1,7 +0,0 @@
-source /opt/ros/humble/setup.bash
-source install/setup.bash
-colcon build --cmake-args -DBUILD_TESTING=ON
-
-export LIBGL_ALWAYS_SOFTWARE=1
-
-ros2 launch ros_gz_example_bringup diff_drive.launch.py
--- robot/src/build/ros_gz_example_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kylor/setup/INF3995-Templates/project_ws/src/install/ros_gz_example_description/share/ros_gz_example_description/package.sh"
