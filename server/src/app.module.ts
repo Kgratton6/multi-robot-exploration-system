3efced497bf0649674f60900b428fc7132e5773b
Rafik Boumila <rafik6639@gmail.com>
2025-01-28 12:13:17 -0500

    Creating mission services

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MissionModule } from './mission.module'; //Makes MissionModule available to the whole application. The API should be available at: GET http://localhost:3000/missions/status POST http://localhost:3000/missions/send
 @Module({
-  imports: [],
+  imports: [MissionModule],
   controllers: [AppController],
   providers: [AppService],
 })
