36a2ce9b979a96c8e263052bf1e8e5b2d9a9bc76
kgratton6 <kevin-santaigo.gratton-fournier@polymtl.ca>
2025-02-06 10:00:05 -0500

    test for running the robot v1

---
+++ test/catkin_ws/build/limo_base/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevin/INF3995-102/test/catkin_ws/src/limo_ros2/limo_base
+# Build directory: /home/kevin/INF3995-102/test/catkin_ws/build/limo_base
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ test/catkin_ws/build/limo_base/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/kevin/INF3995-102/test/catkin_ws/install/limo_msgs/share/limo_msgs/package.sh"
