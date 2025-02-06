b2594a93318a5be389d8ff702498cc1862a2b7e7
kgratton6 <kevin-santaigo.gratton-fournier@polymtl.ca>
2025-02-06 15:50:05 -0500

    clean up of the setup

--- robot/build_simulator.sh
+++ robot/build_simulator.sh
@@ -1,7 +1,7 @@
+# À COMPLETER
+
 source /opt/ros/humble/setup.bash
+export LIBGL_ALWAYS_SOFTWARE=1
 colcon build --cmake-args -DBUILD_TESTING=ON
 source install/setup.bash
-
-export LIBGL_ALWAYS_SOFTWARE=1
-
-ros2 launch ros_gz_example_bringup diff_drive.launch.py
+# launch la simul = ros2 launch ros_gz_example_bringup diff_drive.launch.py
