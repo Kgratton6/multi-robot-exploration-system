1ae05d89d554ce19c16b44a86418781362f733c4
kgratton6 <kevin-santaigo.gratton-fournier@polymtl.ca>
2025-02-11 08:33:13 -0500

    changing sound path

--- robot/robot/src/identification/identification/identification.py
+++ robot/robot/src/identification/identification/identification.py
@@ -14,7 +14,7 @@ class IdentifyNode(Node):
     def identify_callback(self, msg):
         self.get_logger().info("Identification demandée : lancement du son.")
         # Chemin absolu vers le fichier audio (adapté à votre installation)
-        sound_file = 'robot/common/son_identification.wav'
+        sound_file = '/home/equipe102/Desktop/INF3995-102/robot/common/son_identification.wav'
         if os.path.exists(sound_file):
             # Lancement de la commande pour jouer le son (ici, 'aplay' est utilisé pour Linux)
             subprocess.Popen(['aplay', sound_file])
