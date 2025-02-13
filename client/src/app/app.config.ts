713f46f360b24fe4b138d86fb41855d1ad6f009a
Yassine Abassi <yassine020528@gmail.com>
2025-02-13 14:05:22 -0500

    client restored

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(withFetch()), // Utilisation de fetch pour de meilleures performances SSR
+    provideHttpClient(),
+    provideAnimations()
   ]
 };
