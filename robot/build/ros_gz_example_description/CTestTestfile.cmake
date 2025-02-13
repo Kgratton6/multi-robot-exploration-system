b6258758e1e8189031670586097eb887b1d36ae1
kgratton6 <kevin-santaigo.gratton-fournier@polymtl.ca>
2025-02-13 18:44:53 -0500

    communication frondend to robot finished

--- robot/build/ros_gz_example_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kylor/INF3995-102/robot/src/ros_gz_example_description
-# Build directory: /home/kylor/INF3995-102/robot/build/ros_gz_example_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
