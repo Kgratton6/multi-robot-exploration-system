1ea571187917133182cb258f1832a591a4ac2e95
kgratton6 <kevin-santaigo.gratton-fournier@polymtl.ca>
2025-02-13 10:48:10 -0500

    changes to the limo base 3

--- robot/limo/src/limo_ros2/limo_base/launch/limo_base.launch.py
+++ robot/limo/src/limo_ros2/limo_base/launch/limo_base.launch.py
@@ -1,54 +1,81 @@
+import os
 import launch
 import launch_ros
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    # Declare launch arguments
-    namespace_arg = DeclareLaunchArgument(
-        'id', default_value='102robot1',
-        description='Namespace for the robot'
+
+    id_arg = DeclareLaunchArgument(
+        'id',
+        default_value='default_id',
+        description='Namespace ID for the robot'
     )
 
     port_name_arg = DeclareLaunchArgument('port_name', default_value='ttyTHS1',
-                                          description='USB bus name, e.g. ttyTHS1')
+                                         description='usb bus name, e.g. ttyTHS1')
     odom_frame_arg = DeclareLaunchArgument('odom_frame', default_value='odom',
-                                           description='Odometry frame ID')
+                                           description='Odometry frame id')
     base_link_frame_arg = DeclareLaunchArgument('base_frame', default_value='base_link',
-                                                description='Base link frame ID')
+                                                description='Base link frame id')
     odom_topic_arg = DeclareLaunchArgument('odom_topic_name', default_value='odom',
                                            description='Odometry topic name')
-    odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', default_value='true',
-                                        description='Publish odometry TF (true/false)')
+    odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', default_value='True',
+                                           description='Odometry topic name')
+
+    # is_scout_mini_arg = DeclareLaunchArgument('is_scout_mini', default_value='false',
+    #                                       description='Scout mini model')
+    # is_omni_wheel_arg = DeclareLaunchArgument('is_omni_wheel', default_value='false',
+    #                                       description='Scout mini omni-wheel model')
+
+    # simulated_robot_arg = DeclareLaunchArgument('simulated_robot', default_value='false',
+    #                                                description='Whether running with simulator')
     sim_control_rate_arg = DeclareLaunchArgument('control_rate', default_value='50',
                                                  description='Simulation control loop update rate')
-
-    # Define the Limo base node
-    limo_base_node = Node(
+    
+    limo_base_node = launch_ros.actions.Node(
         package='limo_base',
-        executable='limo_base',
+        executable='limo_base',  #foxy executable='limo_base',
         output='screen',
         emulate_tty=True,
         namespace=LaunchConfiguration('id'),
+        remappings=[
+            ('cmd_vel', [LaunchConfiguration('id'), '/cmd_vel']),
+            ('imu', [LaunchConfiguration('id'), '/imu']),
+            ('limo_status', [LaunchConfiguration('id'), '/limo_status']),
+            ('odom', [LaunchConfiguration('id'), '/odom']),
+            ('parameter_events', [LaunchConfiguration('id'), '/parameter_events']),
+            ('rosout', [LaunchConfiguration('id'), '/rosout']),
+            ('tf', [LaunchConfiguration('id'), '/tf']),
+        ]
         parameters=[{
-            'port_name': LaunchConfiguration('port_name'),
-            'odom_frame': LaunchConfiguration('odom_frame'),
-            'base_frame': LaunchConfiguration('base_frame'),
-            'odom_topic_name': [LaunchConfiguration('id'), '/', LaunchConfiguration('odom_topic_name')],
-            'pub_odom_tf': LaunchConfiguration('pub_odom_tf'),
-            'control_rate': LaunchConfiguration('control_rate'),
-        }]
-    )
+                # 'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time'),
+                'port_name': launch.substitutions.LaunchConfiguration('port_name'),                
+                'odom_frame': launch.substitutions.LaunchConfiguration('odom_frame'),
+                'base_frame': launch.substitutions.LaunchConfiguration('base_frame'),
+                'odom_topic_name': launch.substitutions.LaunchConfiguration('odom_topic_name'),
+                # 'is_scout_mini': launch.substitutions.LaunchConfiguration('is_scout_mini'),
+                # 'is_omni_wheel': launch.substitutions.LaunchConfiguration('is_omni_wheel'),
+                # 'simulated_robot': launch.substitutions.LaunchConfiguration('simulated_robot'),
+                'pub_odom_tf': launch.substitutions.LaunchConfiguration('pub_odom_tf'),
+                'control_rate': launch.substitutions.LaunchConfiguration('control_rate'),
+        }])
 
     return LaunchDescription([
-        namespace_arg,
-        port_name_arg,
+        id_arg,
+        port_name_arg,        
         odom_frame_arg,
         base_link_frame_arg,
         odom_topic_arg,
         odom_tf_arg,
+        # is_scout_mini_arg,
+        # is_omni_wheel_arg,
+        # simulated_robot_arg,
         sim_control_rate_arg,
         limo_base_node
     ])
